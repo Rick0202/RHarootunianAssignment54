55a7d61ad928f2fe1a0e22202ea1e6a9ece1f4e6
Rick0202 <richard.harootunian@gmail.com>
2024-10-06 02:01:19 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    
+  
+
+    System.out.println("Miles        Kilometers");
+    for(int x = 1; x < 11; x++) {
+      double k = x * 1.609;
+      System.out.println("  " + x + "             " + k);
+}
+}
+}
